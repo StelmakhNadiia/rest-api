87a8535245a1b046bdfd9d082ece83cde65c9d59
NadiiaStelmakh <nadiaste13@gmail.com>
2026-05-18 23:35:15 +0300

    Lab3: Configure dynamic DATABASE_URL for Render

--- database/db.py
+++ database/db.py
@@ -1,7 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/library"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://postgres:postgres@db:5432/library"
+)
+
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
